06b44ee357350d581818a76dc1958d3927da51e3
sargiss987 <sargiss987@gmail.com>
2025-05-01 12:11:15 +0400

    first commit

---
+++ src/test/java/am/shavigh/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package am.shavigh.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
